bf8cb898a4ad66a3051739fdf678875d4168a380
César Guerra <warderer@gmail.com>
2023-07-04 21:21:55 -0600

    ✨ Hola Mundo desde Vite

---
+++ 01-vite-vanilla/src/components/HolaMundo.js
@@ -0,0 +1,6 @@
+const HolaMundo = (nombre) => `
+    <h1>Hola ${nombre}</h1>
+    <p>Estamos aprendiendo a hacer componentes con ayuda de las Arrow Functions y Template Strings </p>
+`
+
+export default HolaMundo
--- 01-vite-vanilla/src/main.js
+++ 01-vite-vanilla/src/main.js
@@ -1,8 +1,9 @@
 import './styles/style.css'
+import HolaMundo from './components/HolaMundo'
 
 document.querySelector('#app').innerHTML = `
   <div>
-
+    ${HolaMundo('Eduardo')}
   </div>
 `
 
