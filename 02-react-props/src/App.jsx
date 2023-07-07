cf87344ecd5952d204150d2697b70009cf33f23d
DoloresPGz <dolores.pgz@gmail.com>
2023-07-06 20:58:50 -0600

    Proyecto init y props

---
+++ 02-react-props/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import HolaMundo from './components/HolaMundo'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Hola G24</h1>
+      <HolaMundo nombre ="Pedro" apellido ="Lopez"/>
+      <HolaMundo nombre ="Juan" apellido ="Martinez"/>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
---
+++ 02-react-props/src/components/HolaMundo.jsx
@@ -0,0 +1,13 @@
+const HolaMundo = ({nombre, apellido}) => {
+  return (
+    <h2>Hola {nombre} {apellido}</h2>
+  )
+}
+export default HolaMundo
+
+// const HolaMundo = (props) => {
+//     return (
+//       <h2>Hola {props.nombre} {props.apellido}</h2>
+//     )
+//   }
+//   export default HolaMundo
