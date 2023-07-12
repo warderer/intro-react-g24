4a25ce6f5715c609fe15d94525c03b12cfbc273a
César Guerra <warderer@gmail.com>
2023-07-11 20:53:40 -0600

    ✨ Componente ToDoItem

---
+++ 03-todo-list/src/components/ToDoItem.jsx
@@ -0,0 +1,12 @@
+const ToDoItem = ({ todo, handleDelete }) => {
+  return (
+    <li>{todo}
+      <button
+        className='delete-button'
+        onClick={handleDelete}
+      >Eliminar
+      </button>
+    </li>
+  )
+}
+export default ToDoItem
