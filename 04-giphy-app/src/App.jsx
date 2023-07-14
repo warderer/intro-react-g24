1f74545cd308e064ee52200c8df9ed5e096ebf5c
DoloresPGz <dolores.pgz@gmail.com>
2023-07-13 20:35:30 -0600

    useEffect base

--- 04-giphy-app/src/App.jsx
+++ 04-giphy-app/src/App.jsx
@@ -1,6 +1,20 @@
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App () {
+  const [gifs, setSGifs] = useState([])
+  const APIKEY = import.meta.env.VITE_GIPHY_APP_KEY
+
+  // Las llamadas a API ocurren normalmente en el useEffect
+  useEffect(() => {
+    fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${APIKEY}&limit=30&offset=0&rating=g&bundle=messaging_non_clips`)// url de trending
+      .then(res => res.json())// recibo mi respuesta copleta
+      .then(results => {
+        const { data } = results
+        setSGifs(data)
+      }).catch(err => console.log(err))
+  }, [])
+
   return (
     <>
       <div>
